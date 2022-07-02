e2ec96d18154df47a7c91cbbb3267c3a979e7186
gjrjs31@naver.com <gjrjs31@naver.com>
2022-07-02 11:05:10 +0900

    test: merge Test

--- POPPractice/POPPractice/MacBook.swift
+++ POPPractice/POPPractice/MacBook.swift
@@ -6,14 +6,20 @@
 //
 
 struct Macbook {
-    let allowedChargeWatts: Watt?
-    let storedBatteryLevel: Watt?
+    let allowedChargeWatts: Watt
+    let storedBatteryLevel: Watt
     let maxiumBatteryLevel: Watt = 100
 
+    init(allowedChargeWatts: Watt, storedBatteryLevel: Watt) {
+        self.allowedChargeWatts = allowedChargeWatts
+        self.storedBatteryLevel = storedBatteryLevel
+    }
+    
     mutating func chargeBattery(charger: Chargeable) {
         if storedBatteryLevel == maxiumBatteryLevel {
             print(charger.maximumWattPerHour)
             return
         }
+        print("no")
     }
 }
--- POPPractice/POPPractice/main.swift
+++ POPPractice/POPPractice/main.swift
@@ -10,3 +10,14 @@ import Foundation
 typealias WattPerHour = Int
 typealias Watt = Int
 
+var macBookPro = Macbook(allowedChargeWatts: 120, storedBatteryLevel: 50)
+
+let macBookProCharger = Charger(maximumWattPerHour: 140)
+
+let macBookProCharger2 = macBookProCharger.convert(chargeableWattPerHour: macBookPro.allowedChargeWatts)
+
+print(macBookProCharger2)
+
+let macBookProBattery = macBookPro.chargeBattery(charger: macBookProCharger)
+
+print(macBookProBattery)
