c017119c87a407bc16ff24043906e4fad163d022
gjrjs31@naver.com <gjrjs31@naver.com>
2022-07-02 10:22:50 +0900

    feat: Todo1 요구사항 생성

---
+++ POPPractice/POPPractice/Protocol.swift
@@ -0,0 +1,13 @@
+//
+//  Protocol.swift
+//  POPPractice
+//
+//  Created by 허건 on 2022/07/02.
+//
+
+import Foundation
+
+protocol Chargeable {
+    var maximumWattPerHour: WattPerHour { get }
+    func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour
+}
---
+++ POPPractice/POPPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  POPPractice
+//
+//  Created by 허건 on 2022/07/02.
+//
+
+import Foundation
+
+typealias WattPerHour = Int
+typealias Watt = Int
+
